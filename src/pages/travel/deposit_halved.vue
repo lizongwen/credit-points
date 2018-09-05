cc18cd3c4a7a6d2533a8e2f54df3eef092d8e2cc
Sanm <508231830@qq.com>
2018-09-05 14:27:59 +0800

    Update deposit_halved.vue
    小主 请勿重复提交

--- src/pages/travel/deposit_halved.vue
+++ src/pages/travel/deposit_halved.vue
@@ -11,19 +11,36 @@
     <div class="content-padded button-bottom">
       <button class="btn btn-block btn-deposit-halved btn-golden text-size" @click="getIndexInfo">确认申请</button>
     </div>
+    <!--分享弹出窗 begin-->
+    <mt-popup class="sharePopup" v-model="popupVisible">
+      <div class="top-popup">
+        <div class="model">
+          <img src="../../img/travel/Bitmap.png" />
+        </div>
+        <div class="title-tip">
+          <p>小主</p>
+          <p>请勿重复提交</p>
+        </div>
+      </div>
+      <div class="bottom-popup">
+        <img src="../../img/travel/Slice@2x.png" @click="hiddenShare"/>
+      </div>
+    </mt-popup>
+    <!--分享弹出窗 end-->
   </div>
 </template>
 <script>
-import { Toast } from "mint-ui";
 export default {
   name: "deposit_halved",
   data() {
     return {
-      // TODO
-      // 模态框未做
+      popupVisible: true
     };
   },
   methods: {
+    hiddenShare(){
+      this.popupVisible = false;
+    },
     getIndexInfo: async function() {
       let params = {
         method: "XYX00002",
@@ -40,15 +57,13 @@ export default {
       };
 
       const res = await this.$http.post("/apicenter/rest/post", params);
-      if (res.resultCode == "0000") {
+      if (res.resultCode === "0000") {
         this.$store.commit("travel/setStatus", 1);
         this.$router.push({
           name: 'application'
         });
       } else {
-        Toast({
-          message: res.resultMsg
-        });
+        this.popupVisible = true;
       }
     }
   },
@@ -94,6 +109,41 @@ export default {
     }
     .text-size {
       font-weight: bold;
+    }
+  }
+  .mint-popup{
+    border-radius: px(4);
+  }
+  .sharePopup{
+    width: px(210);
+    height: px(234);
+    .top-popup{
+      .model{
+        img{
+          display: block;
+          margin-top: px(20);
+          width: px(210);
+          height: px(120.5);
+        }
+      }
+      .title-tip{
+        margin: 0 auto;
+        margin-top: px(29.4);
+        p{
+          text-align: center;
+          font-size: px(16);
+        }
+      }
+    }
+    .bottom-popup{
+      background-color: rgba(0,0,0,0);
+      img{
+        margin-top: px(40);
+        display: block;
+        margin-left: px(91);
+        width: px(28);
+        height: px(28);
+      }
     }
   }
 }
